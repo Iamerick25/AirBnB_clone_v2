aea403b3c1c6db07963b7ba52f93d87ac778bf5b
Erick Chinyem <erickchinyem@gmail.com>
2024-03-25 05:17:26 -0700

    All mandatory Tasks

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,76 +1,105 @@
 #!/usr/bin/python3
-""" new class for sqlAlchemy """
+""" Database engine """
+
 import os
-from os import getenv
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
-from models.state import State
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
+from models import base_model, amenity, city, place, review, state, user
+
 
 class DBStorage:
-    """ create tables in environmental"""
+    """handles long term storage of all class instances"""
+    CNC = {
+        'BaseModel': base_model.BaseModel,
+        'Amenity': amenity.Amenity,
+        'City': city.City,
+        'Place': place.Place,
+        'Review': review.Review,
+        'State': state.State,
+        'User': user.User
+    }
+
+    """ handles storage for database """
     __engine = None
     __session = None
 
     def __init__(self):
-        user = getenv("HBNB_MYSQL_USER")
-        passwd = getenv("HBNB_MYSQL_PWD")
-        db = getenv("HBNB_MYSQL_DB")
-        host = getenv("HBNB_MYSQL_HOST")
-        env = getenv("HBNB_ENV")
-
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, passwd, host, db),
-                                      pool_pre_ping=True)
-
-        if (os.getenv('HBNB_ENV') == 'test'):
+        """ creates the engine self.__engine """
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                os.environ.get('HBNB_MYSQL_USER'),
+                os.environ.get('HBNB_MYSQL_PWD'),
+                os.environ.get('HBNB_MYSQL_HOST'),
+                os.environ.get('HBNB_MYSQL_DB')))
+        if os.environ.get("HBNB_ENV") == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
-        """
+        """ returns a dictionary of all objects """
+        obj_dict = {}
         if cls:
-            return self.__session.query(cls).all()
-        else:
-            return {
-                f"{obj.__class__.__name__}.{obj.id}": obj
-                for obj in self.__session.query(Base)
-            }
+            obj_class = self.__session.query(self.CNC.get(cls)).all()
+            for item in obj_class:
+                key = str(item.__class__.__name__) + "." + str(item.id)
+                obj_dict[key] = item
+            return obj_dict
+        for class_name in self.CNC:
+            if class_name == 'BaseModel':
+                continue
+            obj_class = self.__session.query(
+                self.CNC.get(class_name)).all()
+            for item in obj_class:
+                key = str(item.__class__.__name__) + "." + str(item.id)
+                obj_dict[key] = item
+        return obj_dict
+
     def new(self, obj):
-        """add a new element in the table
-        """
+        """ adds objects to current database session """
         self.__session.add(obj)
 
+    def get(self, cls, id):
+        """
+        fetches specific object
+        :param cls: class of object as string
+        :param id: id of object as string
+        :return: found object or None
+        """
+        all_class = self.all(cls)
+
+        for obj in all_class.values():
+            if id == str(obj.id):
+                return obj
+
+        return None
+
+    def count(self, cls=None):
+        """
+        count of how many instances of a class
+        :param cls: class name
+        :return: count of instances of a class
+        """
+        return len(self.all(cls))
+
     def save(self):
-        """save changes
-        """
+        """ commits all changes of current database session """
         self.__session.commit()
+
     def delete(self, obj=None):
-        """delete an element in the table
-        """
-        if obj:
+        """ deletes obj from current database session if not None """
+        if obj is not None:
             self.__session.delete(obj)
-            self.save()
 
     def reload(self):
-        """configuration
-        """
-        Base.metadata.drop_all(self.__engine)
+        """ creates all tables in database & session from engine """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(
-                bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = scoped_session(
+            sessionmaker(
+                bind=self.__engine,
+                expire_on_commit=False))
 
     def close(self):
-        """ calls remove()
+        """
+            calls remove() on private session attribute (self.session)
         """
         self.__session.remove()
--- models/review.py
+++ models/review.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+"""
+Review Class from Models Module
+"""
 import os
-=======
-"""This is the review class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
->>>>>>> ab6c9e07257dacb2061bb50faf4c5c02cfb45cf4
 
 class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
-    __tablename__ = "reviews"
-<<<<<<< HEAD
-
-    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+    """Review class handles all application reviews"""
+    if storage_type == "db":
+        __tablename__ = 'reviews'
+        text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-
-     else:
-         text = ""
-         place_id = ""
-         user_id = ""
-=======
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
->>>>>>> ab6c9e07257dacb2061bb50faf4c5c02cfb45cf4
+    else:
+        place_id = ''
+        user_id = ''
+        text = ''
